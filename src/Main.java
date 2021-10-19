ee04331827bf0b872db74ea4bd329de8f192a4cb
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-19 13:03:09 -0600

    switch statements

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int age = 12;
+		String day = "Sday";
 		
-		if (age >= 18) {
-			System.out.println("You are an adult.");
-		} else {
-			System.out.println ("You are a kid.");
+		switch(day) {
+		case "Sunday": System.out.println("It is Sunday!");
+		break;
+		case "Friday": System.out.println("It is Friday!");
+		break;
+		default: System.out.println("That is not Friday or Sunday.");
+		
 		}
 	}
 
