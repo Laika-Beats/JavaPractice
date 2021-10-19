526578c1192470be54b42dfa6f209deb6d9119b0
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-19 13:12:42 -0600

    logical operators

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String day = "Sday";
+//		int temp = 35;
+//		if(temp > 30) {
+//			System.out.println("It is hot outside");
+//			
+//		} else if (temp >= 20 && temp<=30) {
+//			System.out.println("It is warm outside");
+//			
+//		} else {
+//			System.out.println("It is cold outside");
+//			
+//		}
+		Scanner scanner = new Scanner(System.in);
 		
-		switch(day) {
-		case "Sunday": System.out.println("It is Sunday!");
-		break;
-		case "Friday": System.out.println("It is Friday!");
-		break;
-		default: System.out.println("That is not Friday or Sunday.");
+		System.out.println("You are playing a game. Press q or Q to quit.");
+		String response = scanner.next();
 		
+		if (!response.equals("q") && !response.equals("Q")) {
+			System.out.println("You quit the game");
+		} else {
+			System.out.println("You are still playing the game");
+			
 		}
+		
 	}
 
 }
