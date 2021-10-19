c6618acc222f419c8ce61b50c162dfe871c4909f
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-19 16:23:00 -0600

    java array

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int rows;
-		int columns;
-		String symbol = "";
+		String[] cars = {"Toyota", "Ferari", "Lexus"};
 		
-		System.out.println("Enter # of rows: ");
-		rows = scanner.nextInt();
-		System.out.println("Enter # of columns: ");
-		columns = scanner.nextInt();
-		System.out.println("Enter symbol to use: ");
-		symbol = scanner.next();
 		
-		for (int i=1; i <= rows; i++) {
-			System.out.println();
-			for (int j=1; j<=columns; j++) {
-				System.out.print(symbol);
-			}
+		
+		for (int i = 0; i < cars.length; i++) {
+			System.out.println(cars[i]);
 		}
 		
 	}
