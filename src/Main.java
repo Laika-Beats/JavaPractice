aa4875d46c4f5c37088ffe1b53b1ac6fe4bd50df
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-20 14:40:40 -0600

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,20 @@
-
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String[][] cars = new String[3][3];
 		
-		cars[0][0] = "Lexus";
-		cars[0][1] = "Toyota";
-		cars[0][2] = "Ferari";
-		cars[1][0] = "Cadillac";
-		cars[1][1] = "Cadillac";
-		cars[1][2] = "Cadillac";
-		cars[2][0] = "Lambo";
-		cars[2][1] = "Lambo";
-		cars[2][2] = "Lambo";
+		ArrayList<String> food = new ArrayList<String>();
 		
-		for (int i = 0; i < cars.length; i++ ) {
-			System.out.println();
-			for (int j = 0; j < cars[i].length; j++) {
-				System.out.println(cars[i][j]);
-				
-			}
+		food.add("Pizza");
+		food.add("Cheeseburger");
+		food.add("hotdog");
+		
+		
+		for(int i=0; i < food.size(); i++) {
+			System.out.println(food.get(i));
 		}
-		
-		
-		
-		
-		
+				
 	}
 
 }
