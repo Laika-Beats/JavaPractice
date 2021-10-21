846a21d98c4b5c5b38ef96e2c47b0b168d3b37fd
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-21 10:38:45 -0600

    final keyword

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int a = add(3, 7, 9);
-
-		
-		System.out.println(a);
-		
 		
 	}
-	
-	static int add(int x, int y) {
-		System.out.println("This is overload method 1.");
-		return x + y;
-		
-	}
-	static int add(int x, int y, int z) {
-		System.out.println("This is overload method 2.");
-		return x + y + z;
-		
-	}
-
 }
