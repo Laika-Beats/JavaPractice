0f8120d7c2e5ef634f2944253d97faebe56fb264
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-25 17:03:58 -0600

    my first object

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+
+	String make = "Lexus";
+	String model = "IS350";
+	int year = 2021;
+	String color = "black";
+	double price = 50000.00;
+	
+	void drive() {
+		System.out.println("You drive the car.");
+	}
+	void brake() {
+		System.out.println("You hit the brakes.");
+	}
+
+}
