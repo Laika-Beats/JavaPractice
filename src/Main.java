13d42f1070ed4491d3f9b562f4d2eba3d183caa4
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-25 17:08:44 -0600

    objects

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Car myCar1 = new Car();
+		
+//		System.out.println(myCar.make);
+		myCar1.drive();
+		myCar1.brake();
+		
+		
 		
 		
 	}
