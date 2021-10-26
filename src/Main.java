98295eb39849439da8c2c6453a7bd855093221d9
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-26 14:35:54 -0600

    oop dice roller

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Car car1 = new Car("Lexus", 2021, 500000.00);
-		
-		System.out.println(car1.make);
-		System.out.println(car1.price);
-		car1.drive();
-		
-		
-		
+		DiceRoller diceRoller = new DiceRoller();
 		
 	}
 }
