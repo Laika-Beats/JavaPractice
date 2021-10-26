5f2a59f949f31a2658979ccd238a8e562e539b4a
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-26 15:23:08 -0600

    overloaded constructors

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		DiceRoller diceRoller = new DiceRoller();
+		Pizza pizza = new Pizza("deep dish crust", "tomato", "mozzerella");
+		
+		System.out.println("Here are the ingredients of your pizza: ");
+		System.out.println(pizza.bread);
+		System.out.println(pizza.sauce);
+		System.out.println(pizza.cheese);
 		
 	}
 }
