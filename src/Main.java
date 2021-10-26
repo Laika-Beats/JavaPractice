53d80b1617054a0bae0568da9f9dde134a1e7ef5
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-26 15:42:59 -0600

    array of objects

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Car car = new Car("lexus", 2007, 10000.01);
+		Food[] refrigerator = new Food[3];
 		
-		System.out.println(car.make);
-		System.out.println(car.year);
-		System.out.println(car.price);
-		System.out.println(car);
+		Food food1 = new Food("tacos");
+		Food food2 = new Food("pizza");
+		Food food3 = new Food("yogurt");
+		
+		refrigerator[0] = food1;
+		refrigerator[1] = food2;
+		refrigerator[2] = food3;
+		
+		System.out.println(refrigerator[0].name);
+		System.out.println(refrigerator[1].name);
+		System.out.println(refrigerator[2].name);
 		
 	}
 }
