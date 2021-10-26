680d6bb4a6e8bc0bce04f454f75f80cb82873dac
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-26 15:43:06 -0600

    array of objects

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Food[] refrigerator = new Food[3];
+//		Food[] refrigerator = new Food[3];
 		
 		Food food1 = new Food("tacos");
 		Food food2 = new Food("pizza");
 		Food food3 = new Food("yogurt");
+		
+		Food[] refrigerator = {food1, food2, food3};
 		
 		refrigerator[0] = food1;
 		refrigerator[1] = food2;
