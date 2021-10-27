f72e9f3908a35984ee6ff7e2d272dd8947f2ef24
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-27 13:02:59 -0600

    method override

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Animal animal = new Animal();
 		
-		Car car = new Car();
-		Bicycle bike = new Bicycle();
-		
-		car.go();
-		
-		bike.stop();
-		
-		System.out.println(car.doors);
-		System.out.println(bike.pedals);
+		dog.speak();
+		animal.speak();
 	}
 }
