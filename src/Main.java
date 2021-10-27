d4c2e8b1da1a5c2f52c2f35c516019f12948bfe6
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-27 13:20:06 -0600

    super keyword

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Dog dog = new Dog();
-		Animal animal = new Animal();
 		
-		dog.speak();
-		animal.speak();
+		Hero hero1 = new Hero("Batman", 42, "financial wealth");
+		Hero hero2 = new Hero("Spiderman", 18, "super strength and spider sense");
+		
+		System.out.println(hero1);
+		System.out.println(hero2);
+		
 	}
 }
