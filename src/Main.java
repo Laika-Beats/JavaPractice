43d338b7b819e96d1e2d0f72b127920d771f118d
Laika-Beats <laikabeatsdenver@gmail.com>
2021-10-27 12:48:46 -0600

    static keyword

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Car car1 = new Car("Lexus", "IS350 Sport", 2021, 50000.01);
-		Car car2 = new Car("Subaru", "WRX STI", 2021, 50000.01);
+		Friend friend1 = new Friend("Halston");
+		Friend friend2 = new Friend("Steven");
+//		Friend friend3 = new Friend("Claire");
 		
-		Garage garage = new Garage();
+		Friend.displayFriends();
 		
-//		System.out.println(car2);
-//		System.out.println(car1);
 		
-
-		garage.park(car1);
-}}
+	}
+}
